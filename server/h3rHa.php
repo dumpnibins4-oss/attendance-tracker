7dfe74a9e14aff350f740983ac49e3af0e07cbd5
Teddy Arganda <targanda@lrnph.com>
2026-04-07 11:18:22 +0800

    wow

--- server/h3rHa.php
+++ server/h3rHa.php
@@ -1,26 +1,25 @@
 <?php
-// include "db/conn.php";
+include "db/conn.php";
 
-// try {
-//     $password = '41882';
+try {
+    $password = '41877';
 
-//     // Hash password
-//     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    // Hash password
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
-//     $stmt = $conn->prepare("
-//         INSERT INTO att_track_users (lastName, firstName, middleName, school, address, contactNumber, birthday, gender, department, employee_id, biometrics_id, gmail, position)
-//         VALUES ('Salenga', 'Vince Emanuelle', 'Aguirre', 'Pampanga State University', 'Mambog Hermosa, Bataan', '09123456789', '2004-04-02', 'Male', 'IT Department', '2026-41882', '41882', 'vince.salenga@gmail.com', 'OJT')
-//     ");
-//     $stmt->execute();
-//     echo json_encode(['success' => true, 'message' => 'Account created successfully!']);
+    $stmt = $conn->prepare("
+        INSERT INTO att_track_users (lastName, firstName, middleName, school, address, contactNumber, birthday, gender, department, employee_id, biometrics_id, gmail, position)
+        VALUES ('Arganda', 'Teddy', 'Buyco', 'Pampanga State University', 'San Agustin Lubao, Pampanga', '09123456789', '2001-04-19', 'Male', 'IT Department', '2026-41877', '41877', 'teddyarganda415@gmail.com', 'OJT')
+    ");
+    $stmt->execute();
+    echo json_encode(['success' => true, 'message' => 'Account created successfully!']);
 
-//     $stmt = $conn->prepare("
-//         INSERT INTO att_track_login_credentials (biometrics_id, password)
-//         VALUES ('41882', '$hashed_password')
-//     ");
-//     $stmt->execute();
-//     echo json_encode(['success' => true, 'message' => 'Account created successfully!']);
-// }
-// catch (Exception $e) {
-//     echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-// }
+    $stmt = $conn->prepare("
+        INSERT INTO att_track_login_credentials (biometrics_id, password)
+        VALUES ('41877', '$hashed_password')
+    ");
+    $stmt->execute();
+    echo json_encode(['success' => true, 'message' => 'Account created successfully!']);
+} catch (Exception $e) {
+    echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+}
